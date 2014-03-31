fbad802ad7dffa0546af93a65fb578ca011d1f28
Daniele Olmisani <daniele.olmisani@gmail.com>
2014-03-31 21:32:35 +0200

    added LennaTitleScreen

--- src-roms/bytetoaster/LennaTitleScreen.java
+++ src-roms/bytetoaster/LennaTitleScreen.java
@@ -3,7 +3,6 @@ package bytetoaster;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
